9cc51eb5ad6fcc8279b69333c35df106c43ee624
DUSS007 <119634900+DUSS007@users.noreply.github.com>
2024-03-05 20:06:05 -0300

    Add files via upload

---
+++ Move_File.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+
+fromdir ="C:/Users/vurba/Downloads"
+todir = "C:/Users/vurba/Desktop/imagens_baixadas/Arquivos_Documentos"
+
+list_of_files = os.listdir(fromdir)
+print(list_of_files)
